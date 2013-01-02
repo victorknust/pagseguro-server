232bfa9edbf9c1c87b5956d83fb5c9172c32a602
root <root@ubuntu.utd>
2013-01-02 17:57:55 -0200

    update

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+
+# Verificando permissao de root.
+if [ "`id -u`" != 0 ]; then
+	echo "E necessario ter permissao de root para executar este procedimento."
+	exit 1
+fi
+# Verificando permissao de root.
+
+# Verificando diretorio de trabalho.
+ROOT=/root/pagseguro-server
+if [ `pwd` != $ROOT ]; then
+	echo 'O diretorio de instalacao deve ser "/root/pagseguro-server".'
+	exit 1
+fi
+# Verificando diretorio de trabalho.
+
+# Instalando o puppet.
+PUPPET=`dpkg -l | grep puppet | wc -l`
+if [ $PUPPET == 0 ]; then
+	apt-get update
+	apt-get install -y puppet
+fi
+# Instalando o puppet.
+
