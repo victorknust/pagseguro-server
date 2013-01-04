b862cfd32075541d86783b47509ef0b831eb5d0a
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-04 17:22:34 -0200

    update do S.O. #8

--- manifests/01-update.pp
+++ manifests/01-update.pp
@@ -2,5 +2,17 @@ class update {
 	
 	notify {"Atualizando o sistema operacional: $operatingsystem":}
 	
+	case $operatingsystem {
+		Debian,Ubuntu: {
+			$command = "apt-get update && apt-get -y upgrade"
+		}
+		CentOS,RedHat: {
+			$command = "yum -y update"
+		}
+	}
+	
+	exec {"$command":
+		path => "/usr/bin"
+	}
 }
 class {"update":}
