6bef5a4a53bafdf0986e9d50d7795376e3ed7206
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-04 18:34:23 -0200

    update de S.O. #8

--- manifests/01-update.pp
+++ manifests/01-update.pp
@@ -2,19 +2,27 @@ class update {
 	notify {"Atualizando o sistema operacional: $operatingsystem":}
 	case $operatingsystem {
 		Debian,Ubuntu: {
+			$file = "/etc/apt/update_initiator"
+			file {"$file":
+				ensure  => present,
+				content => "",
+				owner   => root,
+				group   => root,
+				mode    => 0644,
+			}
 			exec {"apt-get update":
-				path => "/usr/bin"
+				path => "/usr/bin",
 			}
-			exec {"apt-get -f install":
-				path => "/usr/bin"
-			}
-			exec {"apt-get -y upgrade":
-				path => "/usr/bin"
+			exec {"apt-get -y dist-upgrade":
+				path        => "/usr/bin",
+				#refreshonly => true,
+				#subscribe   => File["$file"],
+				#timeout     => 3600,
 			}
 		}
 		CentOS,RedHat: {
 			exec {"yum -y update":
-				path => "/usr/bin"
+				path => "/usr/bin",
 			}
 		}
 	}
