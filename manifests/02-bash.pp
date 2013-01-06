8735b91f656501ae45ff355279459178986261c9
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-06 18:49:13 -0200

    configurando /etc/skel #10

--- manifests/02-bash.pp
+++ manifests/02-bash.pp
@@ -1,7 +1,58 @@
 class bash {
-	notify {"Configurando o ambiente bash para o sistema: $operatingsystem":}
-	$root = file("/tmp/root")
+	notify {"Configurando o ambiente bash para o sistema":}
 	
-	#Comentário
+	# Configurando o /etc/skel.
+	file {"/etc/skel":
+		ensure => directory,
+		owner  => root,
+		group  => root,
+		mode   => 0755,
+	}
+	file {"/etc/skel/tmp":
+		ensure => directory,
+		owner  => root,
+		group  => root,
+		mode   => 0700,
+	}
+	file {"/etc/skel/.ssh":
+		ensure => directory,
+		owner  => root,
+		group  => root,
+		mode   => 0700,
+	}
+	file {"/etc/skel/.Xauthority":
+		ensure => present,
+		owner  => root,
+		group  => root,
+		mode   => 0600,
+	}
+	file {"/etc/skel/.bash_history":
+		ensure => present,
+		owner  => root,
+		group  => root,
+		mode   => 0600,
+	}
+	file {"/etc/skel/.profile":
+		ensure => present,
+		owner  => "root",
+		group  => "root",
+		mode   => 0700,
+		source => "$root/manifests/files/skel/.profile"
+	}
+	file {"/etc/skel/.bashrc":
+		ensure => present,
+		owner  => "root",
+		group  => "root",
+		mode   => 0700,
+		source => "$root/manifests/files/skel/.bashrc"
+	}
+	file {"/etc/skel/.bash_logout":
+		ensure => present,
+		owner  => "root",
+		group  => "root",
+		mode   => 0700,
+		source => "$root/manifests/files/skel/.bash_logout"
+	}
+	# Configurando o /etc/skel.
 }
 class {"bash":}
