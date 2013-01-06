65271ab909abe8b35dd5dd0c35190ba25933df2d
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-06 02:09:40 -0200

    update

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-clear
-pwd > /tmp/root
-
 # Verificando permissao de root.
 if [ "`id -u`" != 0 ]; then
 	echo "É necessario ter permissão de root para executar este procedimento."
 	exit 1
 fi
 # Verificando permissao de root.
+
+clear
+pwd > /tmp/root
 
 # Configurando o FQDN.
 sh `dirname $0`/manifests/scripts/fqdn-configure.sh
