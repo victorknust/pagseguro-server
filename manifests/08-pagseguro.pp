739e0b9322c2a5da51a3b0e1a8f0fb9d31658357
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-08 18:48:46 -0200

    sistema do PagSeguro #18

--- manifests/08-pagseguro.pp
+++ manifests/08-pagseguro.pp
@@ -6,5 +6,13 @@ class pagseguro {
 		ensure => installed
 	}
 	
+	file {"/usr/local/pagseguro":
+		ensure => directory,
+		owner  => root,
+		group  => root,
+		mode   => 0755,
+		source => "$root/manifests/files/pagseguro/server",
+	}
+	
 }
 class {"pagseguro":}
